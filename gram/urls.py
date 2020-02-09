bfedc5606c1585f8573dca1501ce73794a4aa47e
murathe <isaacsm07p@gmail.com>
2020-02-09 17:02:26 +0300

    Main route for sighup

--- gram/urls.py
+++ gram/urls.py
@@ -3,3 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('', views.Signup, name='signup'),
+
+]
