80b56151cb0856fb6ccfe4d7080dd0c24701fd2f
murathe <isaacsm07p@gmail.com>
2020-02-09 10:14:19 +0300

    Def index route

--- gram/views.py
+++ gram/views.py
@@ -4,3 +4,11 @@ from .forms import
 from django.core.mail import send_mail
 from .models import Profile, Comment, Post
 # Create your views here.
+
+@login_required(login_url='/login/')
+def Index(request):
+    current_user = request.user
+    posts = Post.get_posts()
+    comments = Comment.get_comments()
+
+    
