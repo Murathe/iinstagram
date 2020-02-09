a7694c4fdda8d5bcbf126112c7b27137d8bfab18
murathe <isaacsm07p@gmail.com>
2020-02-09 17:08:36 +0300

    Define signup form func

--- gram/forms.py
+++ gram/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 from .models import Post, Profile, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text None
