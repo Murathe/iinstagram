ee4f8ac0089bfe035e9907ebaef9777008e8133f
murathe <isaacsm07p@gmail.com>
2020-02-09 22:24:06 +0300

    Change db name

--- innstagram/settings.py
+++ innstagram/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'innstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gram',
     }
 }
 
