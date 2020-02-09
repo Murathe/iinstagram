64891c27e894eb90fb0457551b6676401aa40f5d
murathe <isaacsm07p@gmail.com>
2020-02-09 09:51:35 +0300

    Image class

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class IMage(models.Model):
+    image = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=None)
+    image_caption = models.CharField(max_length=50)
+    
