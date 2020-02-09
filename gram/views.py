41b338607669877bc16026f0ef826b4e989c58cb
murathe <isaacsm07p@gmail.com>
2020-02-09 10:11:52 +0300

    Import models clasess

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from .forms import 
+from django.core.mail import send_mail
+from .models import Profile, Comment, Post
 # Create your views here.
