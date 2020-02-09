d59cea39d975b5d59d47035e9d04762c1e2c6835
murathe <isaacsm07p@gmail.com>
2020-02-09 22:22:19 +0300

    INstall database depend

--- gram/models.py
+++ gram/models.py
@@ -1,31 +1,53 @@
 from django.db import models
+import datetime as dt
 from django.contrib.auth.models import User
-# Create your models here.
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=None)
-    image_caption = models.CharField(max_length=50)
-    likes = models.PositiveIntegerField(value = 0)
-    comments = models.TextField(max_length= 30 )
-    image_name = models.CharField(max_length=50)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+# Create your models here.
+class Post(models.Model):
+    caption = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post_image = models.ImageField(upload_to = 'posts/')
 
     @classmethod
-    def get_images(cls):
-        images = cls.objects.all()
-        return images
+    def get_posts(cls):
+        posts = cls.objects.all()
+        return posts
 
-    def save_image(self):
+    def save_post(self):
         self.save()
 
-class Profile(models.Model):
-    profile_image = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=None)
-    bio = models.TextField(max_length=100)
+    def delete_post(self):
+        self.delete()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    comment = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     @classmethod
+    def get_comments(cls):
+        comments = cls.objects.all()
+        return comments
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,)
+    profile_photo = models.ImageField(upload_to = 'posts/')
+    bio = models.TextField(max_length=255)
+
     def save_profile(self):
         self.save()
 
     def delete_profile(self):
         self.delete()
-        
+
+    def updateProfile(sender, **kwargs):
+        if kwargs['created']:
+            profile = Profile.objects.created(user=kwargs['instance'])
+
+            post_save.connect(Profile, sender=User)
+            
+            
+    
--- gram/urls.py
+++ gram/urls.py
@@ -13,3 +13,6 @@ urlpatterns = [
     re_path(r'^profile/', views.profile, name='profile'),
     re_path(r'^updateprofile/', views.update_profile, name='update_profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
